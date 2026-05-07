e38447bb47d918716205202c888a7121c439bedd
umarorakzai <umarorakzai315@gmail.com>
2026-05-07 08:44:53 -0700

    DataTypes Conversion done

---
+++ 01_basics/03_ConversionAndOperation.js
@@ -0,0 +1,23 @@
+let age = "28";
+
+console.log(typeof age);
+
+let valueNumber = Number(age)
+console.log( typeof valueNumber);
+
+
+let isLogedIn = 1;
+ 
+console.log(isLogedIn);
+
+let booleinValue = Boolean(isLogedIn)
+
+console.log(booleinValue);
+
+let someNumber = 33
+let toString = String(someNumber)
+console.log(typeof toString);
+
+
+
+

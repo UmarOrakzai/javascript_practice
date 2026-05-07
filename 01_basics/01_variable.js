a168016c6e8912d8fb31249661f82364fa6fa910
umarorakzai <umarorakzai315@gmail.com>
2026-05-07 08:28:09 -0700

    DataTypes done

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,5 +1,15 @@
+//variable is used to store data let, var, const are keyword to create 
+//variable var and let value can be changed but var have scope problem 
+//const value cannot changed
+//now most use var and const 
+
+//const variable 
 const userId = 123242;
+
+//let variable 
 let userEmail = "umarorakzai@gmail.com"
+
+// var variable
 var  userPassword =  12345;
 
 

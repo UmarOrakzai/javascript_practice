f588c37095f3584ba4987289ae1116cf55237b1d
umarorakzai <umarorakzai315@gmail.com>
2026-05-08 05:25:10 -0700

    String

---
+++ 01_basics/04_String.js
@@ -0,0 +1,11 @@
+const name = "Umar"
+const accountId = 1059;
+
+console.log(`My Name is ${name} and account id is ${accountId}`);
+
+
+const gameName = new String ("Cricket")
+
+console.log(gameName[0]);
+
+

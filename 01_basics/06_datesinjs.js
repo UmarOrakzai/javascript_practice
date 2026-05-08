dd36f382d06f0dc3862bc6973ff6b9e1f22b6479
umarorakzai <umarorakzai315@gmail.com>
2026-05-08 08:13:00 -0700

    Dates and Time

---
+++ 01_basics/06_datesinjs.js
@@ -0,0 +1,24 @@
+//Dates
+
+
+
+
+const myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+//Type of data
+
+console.log(typeof myDate);
+const myAge = 1997
+console.log("My Age is ", myDate.getFullYear()-myAge);
+
+
+
+
+ 
+
+
+

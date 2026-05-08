4d505f2a00f40876656c9d5338254ba1dcfb98a3
umarorakzai <umarorakzai315@gmail.com>
2026-05-08 06:14:03 -0700

    String Done

--- 01_basics/04_String.js
+++ 01_basics/04_String.js
@@ -4,8 +4,28 @@ const accountId = 1059;
 console.log(`My Name is ${name} and account id is ${accountId}`);
 
 
-const gameName = new String ("Cricket")
+const gameName = new String ("cricket")
 
 console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('t'));
+console.log(gameName.substring(0,4));
+console.log(gameName.slice(-7,5));
+
+const newString = "     umar   "
+
+console.log(newString);
+console.log(newString.trim());
+
+const userName = "Umar orakzai"
+
+console.log(userName.replace('orakzai', 'khan'));
+
+
+
+
+
+
 
 

4b59dc6add690dac4b69ffeb3853728a9f3264b9
umarorakzai <umarorakzai315@gmail.com>
2026-05-08 07:55:16 -0700

    NUmber And Math Done

---
+++ 01_basics/05_numsAndmath.js
@@ -0,0 +1,30 @@
+const score = 100000
+
+console.log(score.toFixed(2));
+
+
+console.log(score.toLocaleString());
+
+
+//+++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++
+
+
+
+console.log(Math);
+console.log(Math.abs(-6));
+console.log(Math.round(5.8));
+console.log(Math.floor(4.5));
+
+console.log(Math.max(2,46,7,8,3,5));
+console.log(Math.min(4,6,84,25,4,8));
+
+const randomNumber = Math.floor(Math.random() *10+1)
+console.log(randomNumber);
+
+
+
+
+
+
+
+

576357d4e212c0e63936a6c4455550300ad5bf2b
umarorakzai <umarorakzai315@gmail.com>
2026-08-09 04:22:21 -0700

    Function updated

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,13 +6,10 @@ myname()
 
 
 
-
-
 function addTwoNum(number1, number2){
   //  console.log(number1 + number2)
 
 }
-
 addTwoNum(5,7)
 
 
@@ -21,25 +18,49 @@ function addTwoNum(number1, number2){
     return number1 + number2
 
 }
-
 const result = addTwoNum(5,7)
 //console.log('Result is:', result);
 
 
 
-
-
 function userLogdinMessage(username){
     if(username === undefined){
-        console.log("Please Enter User Name");
-        return
-        
+      //  console.log("Please Enter User Name");
+        return       
 
     }
-    console.log(`${username} is loged in`);
+//    console.log(`${username} is loged in`);
+
+}
+userLogdinMessage()
+
+
+
+// Rest operator 
+
+
+function CalculateCartPrice(value1, ...price){
+   // console.log(price);
     
 
 }
+CalculateCartPrice(23,45,66,24)
 
 
-userLogdinMessage()
+//Function with object
+
+
+const user ={
+    UserName:'umar',
+    UserCity: 'Hangu',
+    UserEmail:'umarorakzai@gmail.com'
+}
+
+
+function HandleObject(anyObject){
+    console.log(`user name is ${anyObject.UserName}
+    city name ${anyObject.UserCity} user email ${anyObject.UserEmail}`);
+    
+}
+
+HandleObject(user)

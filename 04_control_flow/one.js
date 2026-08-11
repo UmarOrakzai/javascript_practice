588b8fccf50ea5647244aa0a5ccb741588f420ab
umarorakzai <umarorakzai315@gmail.com>
2026-08-11 04:09:03 -0700

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,59 @@
+//if Else
+
+const temprature = 60;
+
+// if (temprature === 50) {
+//     console.log(`Temprature is less then 50 ${temprature}`);
+    
+    
+// } else {
+//     console.log(`Temprature is greater then 50 ${temprature}`);
+    
+    
+// }
+
+// console.log('Executed');
+
+
+// if (temprature === 50) {
+//     console.log(`Temprature is equal to 50: ${temprature}`);
+    
+    
+// } else if(temprature > 50) {
+//     console.log(`Temprature is greater then 50: ${temprature}`);
+    
+    
+// }else if(temprature <  50) {
+//     console.log(`Temprature is less then 50 ${temprature}`);
+    
+    
+// }
+// console.log('Executed');
+
+// const userLogin = true;
+// const debitCard = true;
+// const logedInfromGoogle = true;
+// const logedInfromEmail = false;
+
+// if (userLogin && debitCard) {
+//     console.log('User Allow to Shopping ');
+    
+    
+// } else {
+//     console.log('User Is not Allow to Shopping');
+    
+// }
+
+
+// if (logedInfromGoogle || logedInfromEmail) {
+//     console.log( ' user is login');
+    
+    
+// } else {
+//     console.log('user cannot login');
+    
+// }
+
+
+
+

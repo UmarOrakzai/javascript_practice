23a7512a8cccc7ddc79f1c738ea75f9cc7e1488a
umarorakzai <umarorakzai315@gmail.com>
2026-08-11 04:40:49 -0700

    Truthy conditions

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,33 @@
+const myArray = []
+
+if(myArray.length ===0){
+    console.log('array is empty');
+    
+
+}
+ const myobject = {}
+
+ if(Object.keys(myobject).length === 0){
+    console.log('object is empty');
+    
+ }
+
+ //Nullish Coalescing operator (??): null undefined
+
+ let val1;
+ //val1 = 5 ?? 10
+ val1 = null ?? 10
+
+ console.log(val1);
+
+ //Terniary operator 
+ // condition ? true : false 
+
+
+ const icePrice = 77
+
+ icePrice >= 90 ? console.log("greater then 100") : console.log
+ ('less then 90');
+ 
+ 
+ 

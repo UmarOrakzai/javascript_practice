e11ba9fc8c735c6d6402b8183a998afef196a8e3
umarorakzai <umarorakzai315@gmail.com>
2026-08-12 10:07:35 -0700

    For In Loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const languages = {
+    js: 'javascript',
+    cpp: ' C++',
+    rb: 'ruby',
+    jv: 'java'
+}
+
+
+for (const key in languages) {
+    
+    console.log(`${key} is shortcut for ${languages[key]}`);
+    
+    
+    
+}
+
+
+const programing = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programing) {
+
+    console.log(programing[key]);
+    
+   
+    
+}

75c3a9fc449ab6f18628482d62836226bd4413a4
umarorakzai <umarorakzai315@gmail.com>
2026-08-12 08:26:52 -0700

    While and Do While loo

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+const users = ['umar', 'Ali', 'Ahmad']
+
+let arr = 0
+
+//While Loop
+while (arr < users.length) {
+    console.log(`value is ${users[arr]}`);
+    arr = arr+1;
+    
+    
+}
+
+
+//Do While Loops
+
+let score = 1;
+
+do {
+
+    console.log(`Score is ${score}`);
+    score++
+    
+    
+} while (score < 10);

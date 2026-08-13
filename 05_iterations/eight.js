0def5ec1f1c0fed44bfed5522d9874455f57ed20
umarorakzai <umarorakzai315@gmail.com>
2026-08-13 02:52:04 -0700

    reduce method

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,9 +1,38 @@
 const mynumbers = [1,3,4,5]
 
 
-const mytotal = mynumbers.reduce( function(acc, currentval){
-    return acc + currentval
+// const mytotal = mynumbers.reduce( function(acc, currentval){
+//     return acc + currentval
 
-})
+// })
 
-console.log(mytotal);
+// console.log(mytotal);
+
+// const mytotal = mynumbers.reduce( (acc, currentval)=> acc+currentval, 0)
+
+// /console.log(mytotal);
+
+const Courses = [
+    {
+    courseName: 'javascript',
+    price:     15000
+},
+{
+    courseName: 'C++',
+    price:     10000
+},
+{
+    courseName: 'python',
+    price:     25000
+},
+]
+
+const pricetoPay = Courses.reduce((acc, item)=>{
+    return acc+item.price
+
+},0)
+
+
+console.log(pricetoPay);
+
+

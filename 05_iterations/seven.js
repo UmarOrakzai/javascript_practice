14d4cb486df02c98297becd09f8b80199ab915db
umarorakzai <umarorakzai315@gmail.com>
2026-08-13 01:12:37 -0700

    Map method

---
+++ 05_iterations/seven.js
@@ -0,0 +1,17 @@
+const Numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const myNums =  Numbers.map( (num)=> {
+//     return num+10
+// })
+
+// console.log( myNums);
+
+
+const myNums = Numbers
+                      .map( (num)=> num*10)
+                      .map( (num)=> num+1)
+                      .filter( (num)=> num<=50)
+
+
+                      console.log(myNums);
+                      

43f75abf6a31905b018e34e84f58edbdc4bf0127
umarorakzai <umarorakzai315@gmail.com>
2026-08-22 10:08:34 -0700

    updated

--- 09_advance/weather_project/index.js
+++ 09_advance/weather_project/index.js
@@ -1,16 +1,31 @@
 
 
 const ApiKey = '8f668cc52b1f380be02c5afe85e96991'
-const CityName = 'Hangu';
+let CityName = 'Hangu';
 const city = document.getElementById('city')
+const temp = document.getElementById('temp')
+const description = document.getElementById('description')
+const forms = document.querySelector('form')
+
+
+ 
  async function weatherInfo(){
 
     try {
         const Response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CityName},PK&appid=${ApiKey}&units=metric&lang=ur`)
         const data = await Response.json();
         console.log(data);
-        city.innerHTML = `${CityName}, PK`
+        city.innerHTML = `${data.name}, PK`;
+        console.log(data.main);
+        temp.innerHTML = `${data.main.temp}`
+        console.log(description);
         
+        description.innerHTML = `${data.weather[0].description}`
+        if(data.weather[0].icon === '10n')
+        {
+            document.getElementById('icon').innerHTML = '10n' 
+        }
+              
     } catch (error) {
 
         console.log(error);
@@ -19,4 +34,14 @@ const city = document.getElementById('city')
     }
 
  }
- weatherInfo();
+ forms.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    CityName = document.querySelector('input').value;
+    console.log(CityName);
+    weatherInfo();
+    
+   // console.log(e);
+    
+})
+ 

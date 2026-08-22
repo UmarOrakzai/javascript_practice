83a340952b876f17ec1ef27c75f1acaefe2ffd11
umarorakzai <umarorakzai315@gmail.com>
2026-08-22 07:19:33 -0700

    updated

--- 09_advance/weather_project/index.js
+++ 09_advance/weather_project/index.js
@@ -2,12 +2,14 @@
 
 const ApiKey = '8f668cc52b1f380be02c5afe85e96991'
 const CityName = 'Hangu';
+const city = document.getElementById('city')
  async function weatherInfo(){
 
     try {
         const Response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CityName},PK&appid=${ApiKey}&units=metric&lang=ur`)
         const data = await Response.json();
         console.log(data);
+        city.innerHTML = `${CityName}, PK`
         
     } catch (error) {
 
@@ -17,3 +19,4 @@ const CityName = 'Hangu';
     }
 
  }
+ weatherInfo();
